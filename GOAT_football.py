e8d1d930d8afb708105bcbe9bbbb01fbfa934de5
jcsd84 <jsaraviadrago@gmail.com>
2021-04-09 16:18:41 -0500

    Updating position

--- GOAT_football.py
+++ GOAT_football.py
@@ -7,16 +7,13 @@ cur = conn.cursor()
 cur.execute('DROP TABLE IF EXISTS GOAT')
 
 cur.execute('CREATE TABLE GOAT (Jugador nvarchar(50), Equipo nvarchar(50), Anho nvarchar(50), Certamen nvarchar(50),'
-            'Partidos int, Goles int, Asistencia int)')
+            'Partidos int, Goles int, Asistencia int, Prop_Goles_Partido number)')
 
 data_goat = pd.read_csv('/Users/home/Documents/MP blog 2021/Pele_maradona_Messi.csv', sep='\t')
+data_goat['Prop_Goles_Partido'] = data_goat['Goles']/data_goat['Partidos']
 #print(data_goat.head())
 
 data_goat.to_sql('GOAT', conn, if_exists= 'replace', index=True)
-
-cur.execute('ALTER TABLE GOAT ADD Prop_Goles_Partido int')
-cur.execute('UPDATE GOAT SET Prop_Goles_Partido = COALESCE(Goles,0)/COALESCE(Partidos,0)')
-
 
 #cur.execute('SELECT Jugador, AVG(Goles) FROM GOAT GROUP BY Jugador')
 #cur.execute('SELECT Jugador, AVG(Asistencia) FROM GOAT GROUP BY Jugador')
@@ -25,9 +22,11 @@ cur.execute('UPDATE GOAT SET Prop_Goles_Partido = COALESCE(Goles,0)/COALESCE(Par
 #cur.execute('SELECT Jugador, Equipo, ROUND(AVG(Goles),2) FROM GOAT GROUP BY Jugador, Equipo')
 #cur.execute('SELECT Jugador, Equipo, ROUND(AVG(Goles),2) FROM GOAT WHERE Certamen = "Mundial" GROUP BY Jugador, Equipo')
 #cur.execute('SELECT Jugador, Equipo, SUM(Goles) FROM GOAT WHERE Certamen = "Mundial" GROUP BY Jugador, Equipo')
-#cur.execute('SELECT Jugador, Equipo, SUM(Asistencia) FROM GOAT WHERE Certamen = "Mundial" GROUP BY Jugador, Equipo')
-cur.execute('SELECT Jugador, AVG(Prop_Goles_Partido) FROM GOAT GROUP BY Jugador')
+#cur.execute('SELECT Jugador, SUM(Asistencia) FROM GOAT WHERE Certamen = "Mundial" GROUP BY Jugador')
+#cur.execute('SELECT Jugador, AVG(Prop_Goles_Partido) FROM GOAT GROUP BY Jugador')
+#cur.execute('SELECT Jugador, Anho, Partidos, Prop_Goles_Partido FROM GOAT WHERE Certamen = "Mundial"')
 
+cur.execute('SELECT Jugador, AVG(Prop_Goles_Partido), SUM(Partidos) FROM GOAT WHERE Certamen = "Mundial" GROUP BY Jugador')
 
 for row in cur.fetchall():
     print(row)
