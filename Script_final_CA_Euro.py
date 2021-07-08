130526729730319d14be820ce0954a04f501e736
jcsd84 <jsaraviadrago@gmail.com>
2021-07-08 16:08:22 -0500

    Updating position

--- Script_final_CA_Euro.py
+++ Script_final_CA_Euro.py
@@ -4,29 +4,30 @@ import sqlite3 as sq3
 conn = sq3.connect('Eurocopa_previa_final.sqlite')
 cur = conn.cursor()
 
-cur.execute('DROP TABLE IF EXISTS Eurocopa_previa_final')
+cur.execute('DROP TABLE IF EXISTS Eurocopa_pfinal')
 
-cur.execute('CREATE TABLE Eurocopa_previa_final (Anho int, Partido int, Fase nvarchar(50), '
+cur.execute('CREATE TABLE Eurocopa_pfinal (Anho int, Partido int, '
+            'Fase nvarchar(50), '
             'Equipo nvarchar(50), Score int, Remate int, Remate_arco int, '
             'Posesion int, Pases int, Faltas int, Rival nvarchar(50))')
 
 
 data_euro = pd.read_csv("/Users/home/Documents/MP blog 2021/Data/Eurocopa/Eurocopa_2021_previa_final.csv")
 
-data_euro_breve = pd.Data_frame(data_euro, columns=['Anho', 'Partido', 'Fase',
+data_euro_breve = pd.DataFrame(data_euro, columns=['Anho', 'Partido', 'Fase',
                                                     'Equipo', 'Score', 'Remate',
                                                     'Remate_arco', 'Posesion', 'Pases',
                                                     'Faltas', 'Rival'])
 
-data_euro_breve.to_sql('Eurocopa_previa_final', conn, if_exists = 'replace', index = True)
+data_euro_breve.to_sql('Eurocopa_pfinal', conn, if_exists = 'replace', index = True)
+#print(data_euro_breve.head())
 
+#cur.execute('SELECT Rival, SUM(Score) AS goles_contra FROM Eurocopa_previa_final GROUP BY Rival ORDER BY goles_contra')
+#cur.execute('SELECT Equipo, SUM(Score) AS goles_favor FROM Eurocopa_previa_final GROUP BY Equipo ORDER BY goles_favor DESC')
+cur.execute('SELECT Equipo, SUM(Score) AS goles_favor, SUM(Remate), SUM(Remate_arco), SUM(Faltas), AVG(Pases) FROM Eurocopa_previa_final GROUP BY Equipo ORDER BY goles_favor DESC')
 
-
-
-for row in cur.fetchall:
+for row in cur.fetchall():
     print(row)
 
 conn.commit()
 conn.close()
-
-data_euro.info()
